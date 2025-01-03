443779f1dd72b4bab3c89b0a50b238546dc60565
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-03 14:45:31 +0530

    Update ex2.cpp

--- DSA_Concept_Question/2_Tree/ex2.cpp
+++ DSA_Concept_Question/2_Tree/ex2.cpp
@@ -31,44 +31,95 @@ public:
     {
         root = nullptr;
     }
-    void setRoot(int val)
-    {
-        root.data = val;
-    }
     
+    //Function to insert the Node into the Tree.
     void insertNode(int val)
     {
         //First we have to create the Node with the given value and left and right as nullptr
         //t is the pointer to the newly create node.
         Node* t = new Node(val);
         
-        //In the very beginning root will be node when no node is present in the tree.
+        //In the very beginning root will be nullptr when no node is present in the tree.
         //So root will start pointing to this newly created node.
         if(root == nullptr)
         {
             root = t;
         }
-        else
+        else //when we have atleast one node in the tree.
         {
-            queue<Node*> q;
             //push the root into the queue
-            if(root!=nullptr) q.push();
+            queue<Node*> q;
+            q.push(root);
+            
+            while(!q.empty())
+            {
+                Node* currNode = q.front();
+                q.pop();
+                
+                if(currNode->left ==nullptr)
+                {
+                    currNode->left = t;
+                    return;
+                }
+                else
+                {
+                    q.push(currNode->left);
+                }
+                if(currNode->right == nullptr)
+                {
+                    currNode-> right = t;
+                    return;
+                }
+                else
+                {
+                    q.push(currNode->right);
+                }
+            }
         }
+    }
+    void displayLevelOrder()
+    {
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                Node* currNode = q.front();
+                q.pop();
+                cout<<currNode->data<<" ";
+                
+                if(currNode->left != nullptr)
+                {
+                    q.push(currNode->left);
+                }
+                if(currNode->right != nullptr)
+                {
+                    q.push(currNode-> right);
+                }
+            }
+        }
+        cout<<endl;
     }
     
 };
 
 int main()
 {
-    std::cout<<"Hello World";
     //When we will create the Tree object "new Tree()" ==> one Node* root will be created set as nullptr;
-    Tree t = new Tree();
-    
-    //function to set the root of the tree.
-    t.setRoot(10);
+    Tree t;
     
     //Once root is created then inseert the node in the binary tree.
     t.insertNode(2);
+    t.insertNode(12);
+    t.insertNode(21);
+    t.insertNode(25);
+    t.insertNode(46);
+    t.insertNode(67);
+    
+    t.displayLevelOrder();
 
     return 0;
 }
